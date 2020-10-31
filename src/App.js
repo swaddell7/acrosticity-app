936e3c250cc273582c5af481915e9ffe4a2f0712
swaddell7 <stefani.waddell@hotmail.com>
2020-10-31 15:12:55 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Letter from './Letter';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">ACROSTICITY</header>
+      <Letter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Letter.js
@@ -0,0 +1,22 @@
+import { render } from '@testing-library/react';
+import React, {Component} from 'react';
+
+class Letter extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      id: null
+    }
+  }
+
+  render() {
+    return (
+      <form>
+        <input type="text"></input>
+      </form>
+    )
+  }
+}
+
+
+export default Letter;
