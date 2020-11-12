6c9047fe0fce8e27d5a09626a6c5fdbe6f0f8168
swaddell7 <stefani.waddell@hotmail.com>
2020-11-11 23:08:07 -0600

    added container

---
+++ src/AcrosticContainer.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import Letter from './Letter';
+import AddLetterButton from './AddLetterButton';
+import RemoveLetterButton from './RemoveLetterButton';
+import GenerateButton from './GenerateButton';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      letters: 1
+    }
+  }
+
+  addLetter() {
+    this.setState({ letters: this.state.letters + 1 })
+  }
+
+  removeLetter() {
+    this.setState ({ letters: this.state.letters - 1 })
+  }
+
+  render() {
+    return (
+      <>
+        <Letter />
+        <AddLetterButton />
+        <RemoveLetterButton />
+        <GenerateButton />
+      </>
+    );
+  }
+}
+
+export default App;
--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Letter from './Letter';
 import AddLetterButton from './AddLetterButton';
 import RemoveLetterButton from './RemoveLetterButton';
 import GenerateButton from './GenerateButton';
+import AcrosticContainer from './AcrosticContainer';
 
 
 //pass down props on add letter button for function to add new letter
@@ -10,34 +11,13 @@ import GenerateButton from './GenerateButton';
 //pass down functions as props on add and remove letter components? or maybe this can all be done in app.js
 //use state to loop through and update how many letter components are displayed on the app
 
-class App extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      letters: 1
-    }
-  }
-
-  addLetter() {
-    this.setState({ letters: this.state.letters + 1 })
-  }
-
-  removeLetter() {
-    this.setState ({ letters: this.state.letters - 1 })
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">ACROSTICITY</header>
-        <Letter />
-        <AddLetterButton />
-        <RemoveLetterButton />
-        <GenerateButton />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">ACROSTICITY</header>
+      <AcrosticContainer />
+    </div>
+  );
 }
 
 export default App;
