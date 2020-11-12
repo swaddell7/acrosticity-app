947cdf4b9e4c54f82fb9a5871d56cf2fe589a2c1
swaddell7 <stefani.waddell@hotmail.com>
2020-11-11 22:48:42 -0600

    adding state to app.js

--- src/App.js
+++ src/App.js
@@ -10,16 +10,26 @@ import GenerateButton from './GenerateButton';
 //pass down functions as props on add and remove letter components? or maybe this can all be done in app.js
 //use state to loop through and update how many letter components are displayed on the app
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">ACROSTICITY</header>
-      <Letter />
-      <AddLetterButton />
-      <RemoveLetterButton />
-      <GenerateButton />
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      letters: 1
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">ACROSTICITY</header>
+        <Letter />
+        <AddLetterButton />
+        <RemoveLetterButton />
+        <GenerateButton />
+      </div>
+    );
+  }
 }
 
 export default App;
